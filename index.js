d9dfe2b54d81c7fdd5ee489a15c4271acd62cd18
Rylh655 <79635514+Rylh655@users.noreply.github.com>
2021-02-25 17:09:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["link project"]
+const urls = ["https://rare-voracious-vise.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
